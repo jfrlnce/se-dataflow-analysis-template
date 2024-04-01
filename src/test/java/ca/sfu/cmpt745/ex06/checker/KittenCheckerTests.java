957f77f9b7ecc5f3ef9ef9460ea2bbfc9e867d2d
jfrlnce <147554720+jfrlnce@users.noreply.github.com>
2024-04-01 16:26:01 -0700

    Update KittenCheckerTests.java

--- src/test/java/ca/sfu/cmpt745/ex06/checker/KittenCheckerTests.java
+++ src/test/java/ca/sfu/cmpt745/ex06/checker/KittenCheckerTests.java
@@ -1,320 +1,140 @@
 package ca.sfu.cmpt745.ex06.checker;
 
-import ca.sfu.cmpt745.ex06.examples.*;
-import ca.sfu.cmpt745.ex06.kittens.Kitten;
+import java.util.Map;
+import java.util.EnumSet;
 
 import java.util.List;
 import java.util.ArrayList;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import soot.Body;
+import soot.BodyTransformer;
 import soot.G;
-import soot.PackManager;
-import soot.Scene;
+import soot.Local;
+import soot.RefType;
 import soot.SootClass;
-import soot.Transform;
-import soot.options.Options;
+import soot.SootMethod;
+import soot.Type;
+import soot.Unit;
+import soot.Value;
+import soot.jimple.*;
+import soot.jimple.AbstractExprSwitch;
+import soot.tagkit.LineNumberTag;
+import soot.tagkit.Tag;
+import soot.toolkits.graph.DirectedGraph;
+import soot.toolkits.graph.ExceptionalUnitGraph;
+import soot.toolkits.graph.UnitGraph;
 
+import soot.toolkits.scalar.ForwardFlowAnalysis;
+import java.util.HashMap;
 
-class KittenCheckerTests {
-  static JsonKittenReporter reporter = null;
+public class KittenChecker extends BodyTransformer {
+    final KittenErrorReporter reporter;
 
-  @BeforeEach
-  final void
-  initializeSoot() {
-    reporter  = new JsonKittenReporter();
-
-    soot.G.v().reset();
-    Options.v().set_keep_line_number(true);
-    Options.v().set_soot_classpath(".:target/classes:VIRTUAL_FS_FOR_JDK");
-    final var checker   = new KittenChecker(reporter);
-    final var transform = new Transform("jtp.KittenChecker", checker);
-    PackManager.v().getPack("jtp").add(transform);
-    Scene.v().addBasicClass("ca.sfu.cmpt745.ex06.kittens.Kitten",
-                            SootClass.SIGNATURES);
-  }
-
-  @Test
-  @DisplayName("Check that basic error free behavior can be correct")
-  final void
-  basicCorrectness() {
-    final String exampleName = "ca.sfu.cmpt745.ex06.examples.Test_01_Basic";
-    final String[] args = new String[] { exampleName };
-
-    soot.Main.main(args);
-
-    // First check broad constraints that errors were found on the right lines
-    final var allErrors = reporter.getErrors();
-    assertEquals(0, allErrors.size());
-  }
-
-  @Test
-  @DisplayName("Check that trivially broken code is found")
-  final void
-  basicErrors() {
-    final String exampleName = "ca.sfu.cmpt745.ex06.examples.Test_02_BasicError";
-    final String[] args = new String[] { exampleName };
-
-    soot.Main.main(args);
-
-    // First check broad constraints that errors were found on the right lines
-    final var allErrors = reporter.getErrors();
-    assertEquals(1, allErrors.size());
-
-    final int LINE = 12;
-    final int NUMBER_OF_ERRORS = 1;
-    final var onLine = allErrors.get(LINE);
-    assertNotNull(onLine);
-    assertEquals(NUMBER_OF_ERRORS, onLine.size());
-
-    // With those, we can safely check the specific errors involved
-    final var error = onLine.get(0);
-    assertEquals("$r0", error.variable);
-    assertEquals("running", error.source);
-    assertEquals("sleeping", error.target);
-  }
-
-  @Test
-  @DisplayName("Check that error free `if`s are correct")
-  final void
-  branchingCorrectness() {
-    final String exampleName = "ca.sfu.cmpt745.ex06.examples.Test_03_Conditional";
-    final String[] args = new String[] { exampleName };
-
-    soot.Main.main(args);
-
-    // First check broad constraints that errors were found on the right lines
-    final var allErrors = reporter.getErrors();
-    assertEquals(0, allErrors.size());
-  }
-
-  @Test
-  @DisplayName("Check that broken `if`s are reported")
-  final void
-  branchingError() {
-    final String exampleName = "ca.sfu.cmpt745.ex06.examples.Test_04_ConditionalError";
-    final String[] args = new String[] { exampleName };
-
-    soot.Main.main(args);
-
-    // First check broad constraints that errors were found on the right lines
-    final var allErrors = reporter.getErrors();
-    assertEquals(1, allErrors.size());
-
-    final int LINE = 14;
-    final int NUMBER_OF_ERRORS = 1;
-    final var onLine = allErrors.get(LINE);
-    assertNotNull(onLine);
-    assertEquals(NUMBER_OF_ERRORS, onLine.size());
-
-    // With those, we can safely check the specific errors involved
-    final var error = onLine.get(0);
-    assertEquals("$r0", error.variable);
-    assertEquals("running", error.source);
-    assertEquals("sleeping", error.target);
-  }
-
-  @Test
-  @DisplayName("Check that error free loops are correct")
-  final void
-  loopCorrectness() {
-    final String exampleName = "ca.sfu.cmpt745.ex06.examples.Test_05_Loop";
-    final String[] args = new String[] { exampleName };
-
-    soot.Main.main(args);
-
-    // First check broad constraints that errors were found on the right lines
-    final var allErrors = reporter.getErrors();
-    assertEquals(0, allErrors.size());
-  }
-
-  @Test
-  @DisplayName("Check that broken loops are reported")
-  final void
-  loopError() {
-    final String exampleName = "ca.sfu.cmpt745.ex06.examples.Test_06_LoopError";
-    final String[] args = new String[] { exampleName };
-
-    soot.Main.main(args);
-
-    // First check broad constraints that errors were found on the right lines
-    final var allErrors = reporter.getErrors();
-    assertEquals(1, allErrors.size());
-
-    final int LINE = 12;
-    final int NUMBER_OF_ERRORS = 1;
-    final var onLine = allErrors.get(LINE);
-    assertNotNull(onLine);
-    assertEquals(NUMBER_OF_ERRORS, onLine.size());
-
-    // With those, we can safely check the specific errors involved
-    final var error = onLine.get(0);
-    assertEquals("$r2", error.variable);
-    assertEquals("eating", error.source);
-    assertEquals("plotting", error.target);
-  }
-
-  @Test
-  @DisplayName("Check that error free value propagation are correct")
-  final void
-  propagationCorrectness() {
-    final String exampleName = "ca.sfu.cmpt745.ex06.examples.Test_07_Propagate";
-    final String[] args = new String[] { exampleName };
-
-    soot.Main.main(args);
-
-    // First check broad constraints that errors were found on the right lines
-    final var allErrors = reporter.getErrors();
-    assertEquals(0, allErrors.size());
-  }
-
-  @Test
-  @DisplayName("Check that broken value propagation is reported")
-  final void
-  propagationError() {
-    final String exampleName = "ca.sfu.cmpt745.ex06.examples.Test_08_PropagateError";
-    final String[] args = new String[] { exampleName };
-
-    soot.Main.main(args);
-
-    // First check broad constraints that errors were found on the right lines
-    final var allErrors = reporter.getErrors();
-    assertEquals(1, allErrors.size());
-
-    final int LINE = 14;
-    final int NUMBER_OF_ERRORS = 1;
-    final var onLine = allErrors.get(LINE);
-    assertNotNull(onLine);
-    assertEquals(NUMBER_OF_ERRORS, onLine.size());
-
-    // With those, we can safely check the specific errors involved
-    final var error = onLine.get(0);
-    assertEquals("$r0", error.variable);
-    assertEquals("running", error.source);
-    assertEquals("sleeping", error.target);
-  }
-
-  @Test
-  @DisplayName("Check multiple scenarios together.")
-  final void
-  multipleScenariosTogether() {
-    final String exampleName = "ca.sfu.cmpt745.ex06.examples.Test_09_PuttingTogether";
-    final String[] args = new String[] { exampleName };
-
-    soot.Main.main(args);
-
-    // First check broad constraints that errors were found on the right lines
-    final var allErrors = reporter.getErrors();
-    assertEquals(1, allErrors.size());
-
-    final int LINE = 18;
-    final int NUMBER_OF_ERRORS = 1;
-    final var onLine = allErrors.get(LINE);
-    assertNotNull(onLine);
-    assertEquals(NUMBER_OF_ERRORS, onLine.size());
-
-    // With those, we can safely check the specific errors involved
-    final var error = onLine.get(0);
-    assertEquals("r5", error.variable);
-    assertEquals("running", error.source);
-    assertEquals("sleeping", error.target);
-  }
-
-  @Test
-  @DisplayName("Check independent states for different variables.")
-  final void
-  multipleIndependentStates() {
-    final String exampleName = "ca.sfu.cmpt745.ex06.examples.Test_10_MultipleVariables";
-    final String[] args = new String[] { exampleName };
-
-    soot.Main.main(args);
-
-    // First check broad constraints that errors were found on the right lines
-    final var allErrors = reporter.getErrors();
-    assertEquals(1, allErrors.size());
-
-    final int LINE = 28;
-    final int NUMBER_OF_ERRORS = 1;
-    final var onLine = allErrors.get(LINE);
-    assertNotNull(onLine);
-    assertEquals(NUMBER_OF_ERRORS, onLine.size());
-
-    // With those, we can safely check the specific errors involved
-    final var error = onLine.get(0);
-    assertEquals("$r1", error.variable);
-    assertEquals("playing", error.source);
-    assertEquals("plotting", error.target);
-  }
-
-  @Test
-  @DisplayName("Check merging with multiple variables")
-  final void
-  mergingWithMultipleVariables() {
-    final String exampleName = "ca.sfu.cmpt745.ex06.examples.Test_11_MultipleVariablesMerge";
-    final String[] args = new String[] { exampleName };
-
-    soot.Main.main(args);
-
-    // First check broad constraints that errors were found on the right lines
-    final var allErrors = reporter.getErrors();
-    assertEquals(1, allErrors.size());
-
-    final int LINE = 31;
-    final int NUMBER_OF_ERRORS = 1;
-    final var onLine = allErrors.get(LINE);
-    assertNotNull(onLine);
-    assertEquals(NUMBER_OF_ERRORS, onLine.size());
-
-    // With those, we can safely check the specific errors involved
-    final var error = onLine.get(0);
-    assertEquals("$r1", error.variable);
-    assertEquals("playing", error.source);
-    assertEquals("plotting", error.target);
-  }
-
-  @Test
-  @DisplayName("Check multiple distinct errors in one test.")
-  final void
-  multipleDistinctErrors() {
-    final String exampleName = "ca.sfu.cmpt745.ex06.examples.Test_12_MultipleDistinctErrors";
-    final String[] args = new String[] { exampleName };
-
-    soot.Main.main(args);
-
-    // First check broad constraints that errors were found on the right lines
-    final var allErrors = reporter.getErrors();
-    assertEquals(2, allErrors.size());
-
-    {
-      final int LINE = 42;
-      final int NUMBER_OF_ERRORS = 1;
-      final var onLine = allErrors.get(LINE);
-      assertNotNull(onLine);
-      assertEquals(NUMBER_OF_ERRORS, onLine.size());
-
-      // With those, we can safely check the specific errors involved
-      final var error = onLine.get(0);
-      assertEquals("$r3", error.variable);
-      assertEquals("sleeping", error.source);
-      assertEquals("playing", error.target);
+    KittenChecker(KittenErrorReporter reporter) {
+        this.reporter = reporter;
     }
 
-    {
-      final int LINE = 51;
-      final int NUMBER_OF_ERRORS = 1;
-      final var onLine = allErrors.get(LINE);
-      assertNotNull(onLine);
-      assertEquals(NUMBER_OF_ERRORS, onLine.size());
-
-      // With those, we can safely check the specific errors involved
-      final var error = onLine.get(0);
-      assertEquals("$r1", error.variable);
-      assertEquals("playing", error.source);
-      assertEquals("plotting", error.target);
+    @Override
+    protected void internalTransform(Body body, String phase, Map options) {
+        UnitGraph graph = new ExceptionalUnitGraph(body);
+        KittenAnalysis analysis = new KittenAnalysis(graph, reporter);
     }
-  }
+
+    private class KittenAnalysis extends ForwardFlowAnalysis<Unit, Map<String, String>> {
+        private final UnitGraph graph;
+        private final KittenErrorReporter reporter;
+
+        public KittenAnalysis(UnitGraph graph, KittenErrorReporter reporter) {
+            super(graph);
+            this.graph = graph;
+            this.reporter = reporter;
+            doAnalysis();
+        }
+
+        @Override
+        protected Map<String, String> newInitialFlow() {
+            return new HashMap<>();
+        }
+
+        @Override
+        protected Map<String, String> entryInitialFlow() {
+            return new HashMap<>();
+        }
+
+        @Override
+        protected void merge(Map<String, String> in1, Map<String, String> in2, Map<String, String> out) {
+            out.clear();
+            out.putAll(in1);
+            in2.forEach((key, value) -> out.putIfAbsent(key, value));
+        }
+
+        @Override
+        protected void copy(Map<String, String> source, Map<String, String> dest) {
+            dest.clear();
+            dest.putAll(source);
+        }
+
+        private Map<String, String> lastMethodCalled = new HashMap<>();
+        private Map<String, List<String>> methodSequenceContext = new HashMap<>();
+        @Override
+        protected void flowThrough(Map<String, String> current, Unit unit, Map<String, String> next) {
+            next.putAll(current);
+
+            if (unit instanceof InvokeStmt) {
+                InvokeStmt stmt = (InvokeStmt) unit;
+                InvokeExpr invokeExpr = stmt.getInvokeExpr();
+                if (invokeExpr instanceof InstanceInvokeExpr) {
+                    InstanceInvokeExpr instanceInvokeExpr = (InstanceInvokeExpr) invokeExpr;
+                    String variableName = instanceInvokeExpr.getBase().toString();
+                    String methodName = invokeExpr.getMethod().getName();
+                    String currentState = current.getOrDefault(variableName, "sleeping");
+
+                    String newState = mapMethodNameToState(methodName);
+                    boolean validTransition = isValidTransition(currentState, methodName);
+                      System.out.println("Analyzing: " + variableName + ", Method: " + methodName + 
+                               ", Current State: " + currentState + ", New State: " + newState + 
+                               ", Valid Transition: " + validTransition);
+
+                    methodSequenceContext.computeIfAbsent(variableName, k -> new ArrayList<>()).add(methodName);
+
+                    if (validTransition) {
+                        System.out.println("State Transition: " + variableName + " from " + currentState + " to " + newState);
+                        next.put(variableName, newState);
+                        lastMethodCalled.put(variableName, methodName);
+                    } else {
+                        reporter.reportError(variableName, unit.getJavaSourceStartLineNumber(), newState, currentState);
+                    }
+                }
+            }
+        }
+
+        private boolean isValidTransition(String currentState, String methodName) {
+          switch (methodName) {
+              case "pet":
+                  return !currentState.equals("running") && !currentState.equals("playing");
+              case "tease":
+                  return !currentState.equals("sleeping") && !currentState.equals("eating");
+              case "ignore":
+                  return !currentState.equals("sleeping") && !currentState.equals("eating") && !currentState.equals("playing");
+              case "feed":
+                    return true; 
+              case "scare": 
+                  return true;
+              default:
+                  return true;
+          }
+       }
+
+        private String mapMethodNameToState(String methodName) {
+            switch (methodName) {
+                case "pet": return "sleeping";
+                case "feed": return "eating";
+                case "tease": return "playing";
+                case "ignore": return "plotting";
+                case "scare": return "running";
+                default: return "unknown"; 
+            }
+        }
+    }
 }
+
